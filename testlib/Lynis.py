d51db2e16db32eaaabfbc1185887f5ec89f027a1
Brennan Abbott <babbott.vs@gmail.com>
2020-07-18 12:47:43 -0400

    Committer: Brennan Abbott <babbott.vs@gmail.com>  On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   Lynis.py
    	- added method: git clone lynis (m_git_clone_Lynis)
    		- Auto clone Lynis Git URL

---
+++ testlib/Lynis.py
@@ -0,0 +1,20 @@
+#/bin/Python
+
+
+
+
+def m_git_clone_Lynis():
+    import subprocess
+    lynis_url ="https://github.com/CISOfy/lynis"
+    git_co = subprocess.run(["git","clone",lynis_url]
+
+def main():
+    m_git_clone_Lynis()
+       
+
+
+
+
+
+if __name__ == '__main__':
+    main()
