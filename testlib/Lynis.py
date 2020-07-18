982a16f98711c5e434be2f6f7e1d6c398535dc71
Brennan Abbott <babbott.vs@gmail.com>
2020-07-18 13:42:24 -0400

    On branch master  Changes to be committed: 	modified:   Lynis.py
    Added Method Remove Lynis: m_rm_Lynis
    	- Removes Lynis Directory
    
    Added Argument Parsing for accessing methods
    	- C, --clone for m_git_clone_lynis
    	--clean for m_rm_lynis
    	-r:, --run= For future Lynis execution

--- testlib/Lynis.py
+++ testlib/Lynis.py
@@ -1,20 +1,37 @@
 #/bin/Python
+import subprocess
+import argparse, sys
 
+LYNIS_URL="https://github.com/CISOfy/lynis"
 
 
 
 def m_git_clone_Lynis():
-    import subprocess
-    lynis_url ="https://github.com/CISOfy/lynis"
-    git_co = subprocess.run(["git","clone",lynis_url]
+    lynis_url =LYNIS_URL
+    git_co = subprocess.run(["git","clone",lynis_url])
+    
+def m_rm_Lynis():
+    rm_lynis = subprocess.run(["rm", "-rf", "lynis"])
+    
 
 def main():
-    m_git_clone_Lynis()
-       
+    parser=argparse.ArgumentParser()
+    parser.add_argument("-c", "--clone", help="Auto Clone the latest Lynis from LYNIS_URL to lynis dir",action="store_true")
+    parser.add_argument("-r", "--run", help="Run the specified run suite from lynis dir")
+    parser.add_argument("--clean", help="Remove Lynis Directory",action="store_true")
+    args=parser.parse_args()   
 
-
-
-
-
+    
+    if args.clean:
+        print("Cleaning Lynis Subdirectory...")
+        m_rm_Lynis()
+        print("...Done Cleaning Subdirectory")
+    elif args.clone:
+        print("Cloning....")
+        m_git_clone_Lynis()
+        print("...Done Cloning")
+    elif args.run:
+        pass
+    
 if __name__ == '__main__':
     main()
